a4ae8c158a51d67998f537f8c2ee9e8a5c2ea0e2
Douiba abdelhak <douibaabdelhak2003@gmail.com>
2023-06-15 11:08:09 -0700

    4-puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ *main - Prints "Programming is like building a multilingual
+ *               puzzle
+ *
+ *Return: print 0 if success
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0 ;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
