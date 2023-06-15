4f265efc9121e73f7669d62dd14bb2adf20fd6f5
Douiba abdelhak <douibaabdelhak2003@gmail.com>
2023-06-15 14:25:30 -0700

    quote task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ *main - Print to the standard error the followin text : and that piece of art
+ *             is useful" - Dora Korpar, 2015-10-19
+ *
+ *Return: 1 if the program succes
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+}
