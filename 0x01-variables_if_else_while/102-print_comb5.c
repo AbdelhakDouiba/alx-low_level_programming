8e60b60c49f94f24e763d272eb83a71e6a8d61d2
Douiba abdelhak <douibaabdelhak2003@gmail.com>
2023-06-16 17:05:48 -0700

    done

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ *main - prints all possible combinations of two two-digit numbers.
+ *
+ *Return: 0 if the program success
+ */
+int main(void)
+{
+	int digit_0 = 48, digit_1 = 48, digit_2 = 48, digit_3;
+
+	while (digit_0 < 58)
+	{
+		while (digit_1 < 57)
+		{
+			while (digit_2 < 58)
+			{
+				digit_3 = digit_1 + 1;
+				while (digit_3 < 58)
+				{
+					putchar(digit_0);
+					putchar(digit_1);
+					putchar(' ');
+					putchar(digit_2);
+					putchar(digit_3);
+
+					if ( digit_0 != 57 && digit_1 != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				digit_3++;
+			}
+			digit_2++;
+		}
+		digit_1++;
+	}
+	digit_0++;
+	putchar('\n');
+	return (0);
+}
