879c84dafebf60ff07b68aa4d813875a7a6357ce
Douiba abdelhak <douibaabdelhak2003@gmail.com>
2023-06-20 10:14:43 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*add - return the addition of two numbers
+*@op1: contain the first operand
+*@op2: contain the second operand
+*
+*Return: the addition of the two operands
+*/
+
+int add(int op1, int op2)
+{
+	return (op1 + op2);
+}
