ea5857699f5674dd02858f0b2b25cbd499dba002
Douiba abdelhak <douibaabdelhak2003@gmail.com>
2023-06-20 05:57:49 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,17 @@
 */
 void print_alphabet_x10(void)
 {
-	int i = 0;
+	int i = 0, alpha;
 
-	while(i <10)
+	while (i < 10)
 	{
-		print_alphabet();
+		alpha = 97;
+		while (alpha <= 122)
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
 		i++;
 	}
 }
