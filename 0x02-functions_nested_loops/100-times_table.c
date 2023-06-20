b4d3ed1fbdfae306c0354b3773356b4f4a5fd47f
Douiba abdelhak <douibaabdelhak2003@gmail.com>
2023-06-20 12:00:50 -0700

    done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
 */
 void print_times_table(int n)
 {
-	if (n > 0 && n < 15)
+	if (n >= 0 && n <= 15)
 	{
 		int i = 0, j, times;
 
@@ -45,17 +45,12 @@ void print_times_table(int n)
 						_putchar(' ');
 					}
 				}
-				if (times > 9 && j < n)
+				if (times > 9 && j < n && ((j+1) * i) < 100)
 				{
-					times = (j + 1) * i;
-					if (times < 100)
-					{
 						_putchar(' ');
-					}
 				}
 			j++;
 			}
-
 		_putchar('\n');
 		i++;
 		}
