a9aa3a495978ce7c2ba3a6c5a4c3167badfa13ad
Douiba abdelhak <douibaabdelhak2003@gmail.com>
2023-06-20 06:20:14 -0700

    islowe

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_islower - checks for lowercase character
+*
+*Return: 1 if the character is lowercase, 0 if it is not.
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
