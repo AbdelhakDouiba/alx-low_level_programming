5adcdf309e6c3417fd396f6739116843bc748e4c
Douiba abdelhak <douibaabdelhak2003@gmail.com>
2023-06-20 06:30:16 -0700

    islowe

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 *_islower - checks for lowercase character
+*@c: function parameter it will contains the ASCII code of the character
 *
 *Return: 1 if the character is lowercase, 0 if it is not.
 */
