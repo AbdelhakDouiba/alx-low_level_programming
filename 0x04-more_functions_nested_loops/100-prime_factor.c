139555a04b74910800a8bfa1d5c34489be3bb888
Douiba abdelhak <douibaabdelhak2003@gmail.com>
2023-06-22 11:22:31 -0700

    done

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,44 +1,44 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 #define NUMBER 612852475143
 
 /**
- 
-main - finds and prints the largest prime factor of the number 612852475143,
-followed by a new line.
-Return: 0 if the program succeeds.
-*/
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * Return: 0 if the program succeeds.
+ */
+
 int main(void)
 {
-    unsigned long i = 3, j, k = 0, largest = 1;
+	unsigned long i = 3, j, k = 0, largest = 1;
 
-    while (i * i <= NUMBER)
-    {
-        k = 0;
-        j = 2;
-        while (j * j <= i)
-        {
-            if (i % j == 0)
-            {
-                k = 1;
-                break;
-            }
-            j++;
-        }
-        if (k == 0)
-        {
-            if (NUMBER % i == 0)
-            {
-                largest = i;
-                while (NUMBER % i == 0)
-                    NUMBER /= i;
-            }
-        }
-        i += 2;
-    }
-    if (NUMBER > largest)
-        largest = NUMBER;
+	while (i * i <= NUMBER)
+	{
+		k = 0;
+		j = 2;
+		while (j * j <= i)
+		{
+			if (i % j == 0)
+			{
+				k = 1;
+				break;
+			}
+			j++;
 
-    printf("%lu\n", largest);
-    return (0);
+		}
+		if (k == 0)
+		{
+			if (NUMBER % i == 0)
+			{
+				largest = i;
+				while (NUMBER % i == 0)
+				NUMBER /= i;
+			}
+		}
+		i += 2;
+
+	}
+	if (NUMBER > largest)
+		largest = NUMBER;
+
+	printf("%lu\n", largest);
+	return (0);
 }
