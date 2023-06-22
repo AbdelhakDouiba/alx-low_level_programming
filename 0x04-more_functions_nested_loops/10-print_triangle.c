a705203345ad03e1f0ed7a86f1f3eb8824a12a02
Douiba abdelhak <douibaabdelhak2003@gmail.com>
2023-06-22 09:07:34 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*print_square - prints a triangle, followed by a new line.
+*print_triangle - prints a triangle, followed by a new line.
 *@size: function parameter: contain an integer value
 *
 */
