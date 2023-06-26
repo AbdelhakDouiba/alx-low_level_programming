353c8bb598710a4a10936e819c14b54623d96f49
Douiba abdelhak <douibaabdelhak2003@gmail.com>
2023-06-26 15:13:53 -0700

    I hate pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
 /**
-*rev_string - reverses a string.
-*@s: function argument
-*
-*/
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
 void rev_string(char *s)
 {
-	int lens = 0, i = 0;
 	char tmp;
+	int i, len, len1;
 
-	while (*s != '\0')
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
 	{
-		lens++;
-		s++;
+		len++;
 	}
 
-	while (lens > i)
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[(lens - 1)];
-		s[(lens - 1)] = tmp;
-		lens--;
-		i++;
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
