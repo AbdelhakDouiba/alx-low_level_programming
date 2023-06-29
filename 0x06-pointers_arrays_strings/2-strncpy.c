e8297871ce370f35fafc8843f80b3ff371164835
Abdelhakdouiba <douibaabdelhak2003@gmail.com>
2023-06-29 14:29:37 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-*_strncopy - copies a string
+*_strncpy - copies a string
 *@n: function parameter
 *@src: function parameter
 *@dest: function parameter
 *
 *Return: copies a string
 */
-char *_strncopy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, len;
 
