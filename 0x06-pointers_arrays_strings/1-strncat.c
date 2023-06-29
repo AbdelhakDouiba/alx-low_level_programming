203bc73937373b5b6575bc8550453fad0215c93e
Abdelhakdouiba <douibaabdelhak2003@gmail.com>
2023-06-29 13:40:38 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_strncat - concatenates two strings.
+*@dest: function parameter
+*@src: function parameter
+*@n: function parameter
+*
+*Return: concatenates two strings.
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, lendest, lensrc;
+
+	for (lendest = 0; dest[lendest] != '\0'; lendest++)
+	{
+	}
+	for (lensrc = 0; src[lensrc] != '\0'; lensrc++)
+	{
+	}
+	while (i < n)
+	{
+		dest[lendest + i] = src[i]
+		i++;
+	}
+	return (dest);
+}
