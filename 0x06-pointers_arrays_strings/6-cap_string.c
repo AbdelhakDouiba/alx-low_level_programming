87ae23917418005cca9f71b8d38448bf5939fc31
Abdelhakdouiba <douibaabdelhak2003@gmail.com>
2023-06-29 17:36:59 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -22,16 +22,24 @@ char *cap_string(char *str)
 			str[i] == '{' || str[i] == '}' || str[i] == '\n' ||
 			str[i] == '\t')
 		{
-			if (str[i] == '\t')
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 			{
-				str[i] = ' ';
-				if (str[i + 1] >= 97 && str[i + 1] <= 122)
-				{
-					str[i + 1] -= 32;
-				}
+				str[i + 1] -= 32;
 			}
 		}
 		i++;
+	}
+	i = 0;
+	if (str[i] == ' ' || str[i] == ',' || str[i] == ';' ||
+		str[i] == '.' || str[i] == '!' || str[i] == '?' ||
+		str[i] == '"' || str[i] == '(' || str[i] == ')' ||
+		str[i] == '{' || str[i] == '}' || str[i] == '\n' ||
+		str[i] == '\t')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i + 1] -= 32;
+		}
 	}
 	return (str);
 }
