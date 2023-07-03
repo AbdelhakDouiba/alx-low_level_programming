826d949d33dc3fb1237fb9fa04f02ccf31e15719
Abdelhakdouiba <douibaabdelhak2003@gmail.com>
2023-07-03 08:50:10 -0700

    Done

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,4 +9,27 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
+	unsigned int i = 0, j = 0, count = 0, len;
 
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	while (i <= len)
+	{
+		j = 0;
+		count = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+			}
+			j++;
+		}
+		if (count != 0)
+		{
+			break;
+		}
+		i++;
+	}
+	return (s + i);
