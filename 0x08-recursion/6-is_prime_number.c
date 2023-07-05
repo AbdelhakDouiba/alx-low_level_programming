cee295d94d29a465b1b4a16122fa873726f5da8f
abdelhakdouiba <douibaabdelhak2003@gmail.com>
2023-07-05 10:09:03 -0700

    done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,10 +12,10 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
-	return _is_prime(2, n);
+	return (_is_prime(2, n));
 }
 /**
-*is_prime - returns 1 if the input integer is a prime
+*_is_prime - returns 1 if the input integer is a prime
 *		   number, otherwise return 0.
 *@num: function parameter
 *@i: function paramter
