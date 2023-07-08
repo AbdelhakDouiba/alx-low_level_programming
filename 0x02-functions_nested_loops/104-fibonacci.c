ab5bf2fc4f22286ed9e7d415ed2474e3be0dbdc7
abdelhakdouiba <douibaabdelhak2003@gmail.com>
2023-07-08 06:20:26 -0700

    ME AND NUMBERS ARE FRIENDS

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
 
 /**
-*main - prints the first 98 Fibonacci numbers, starting with 1 and 2,
-*        followed by a new line.
-*
-*Return: 0 if the program success
-*/
+ *main - prints the first 98 Fibonacci numbers, starting with 1 and 2,
+ *       followed by a new line.
+ *
+ *Return: 0 if the program success
+ */
 int main(void)
 {
 	unsigned long int i, tmp, fib_1 = 0, fib_2 = 1;
 
-	for (i = 1 ; i <= 98 ; i++)
+	for (i = 1; i <= 98; i++)
 	{
-		if (i <= 91)
+		tmp = fib_2;
+		fib_2 = fib_1 + fib_2;
+		fib_1 = tmp;
+		printf("%lu", fib_2);
+		if (i != 98)
 		{
-			tmp = fib_2;
-			fib_2 = fib_1 + fib_2;
-			fib_1 = tmp;
-			printf("%lu, ", fib_2);
-		}
-		else
-		{
-			unsigned long int a, b;
-			tmp = fib_2;
-			a = (fib_1 + fib_2) / 10000000000;
-			b = (fib_1 + fib_2) % 10000000000;
-			fib_2 = fib_1 + fib_2;
-			fib_1 = tmp;
-			printf("%lu%lu, ", a, b);
+			printf(", ");
 		}
 	}
 	printf("\n");
