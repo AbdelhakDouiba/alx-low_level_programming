acbf8ee9a7ddecd18bd9b0f39b2d18e57a87e6a8
abdelhakdouiba <douibaabdelhak2003@gmail.com>
2023-07-08 06:40:19 -0700

    ME AND NUMBERS ARE FRIENDS

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,14 +8,30 @@
  */
 int main(void)
 {
-	unsigned long int i, tmp, fib_1 = 0, fib_2 = 1;
+	unsigned long int i, tmp, fib_1 = 0, fib_2 = 1, a, a1, b, b1, tem, tem1;
 
-	for (i = 1; i <= 98; i++)
+	for (i = 1; i < 93; i++)
 	{
 		tmp = fib_2;
 		fib_2 = fib_1 + fib_2;
 		fib_1 = tmp;
 		printf("%lu", fib_2);
+		printf(", ");
+	}
+	a = fib_1 / 1000000000;
+	a1 = fib_1 % 1000000000;
+	b = fib_2 / 1000000000;
+	b1 = fib_2 % 1000000000;
+	for (i; i <= 98; i++)
+	{
+		tem = b;
+		tem1 = b1;
+		b = a + b;
+		b1 = a1 + b1;
+		a = tem;
+		a1 = tem1;
+		printf("%lu", b);
+		printf("%lu", b1);
 		if (i != 98)
 		{
 			printf(", ");
