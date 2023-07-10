2ce6f6ac0382e941e6730b2134596aa484c13bec
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2023-07-10 14:50:57 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,10 +11,28 @@ int **alloc_grid(int w, int h)
 	int i = 0, j, k;
 	int **p;
 
-	p = (int **)malloc(h * sizeof(int));
+	if (h <= 0 || w <= 0)
+	{
+		return (NULL);
+	}
+	p = (int **)malloc(h * sizeof(int *));
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
 	for (k = 0; k < h; k++)
 	{
 		p[k] = (int *)malloc(w * sizeof(int));
+		if (p[k] == NULL)
+		{
+			for(j = 0; j < k; j++)
+			{
+				free(p[j]);
+			}
+			free(p);
+			return (NULL);
+		}
 	}
 	while (i < h)
 	{
