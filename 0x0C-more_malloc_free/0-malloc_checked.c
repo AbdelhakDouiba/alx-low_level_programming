c39de87bddc0d1e7e8c79ace06a0d7a753b96a7d
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2023-07-13 07:24:54 -0700

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,6 +4,7 @@
 *malloc_checked - allocates memory using malloc.
 *@b: function parameter
 *
+*Return: a pointer to the allocated area, else exit with 98
 */
 void *malloc_checked(unsigned int b)
 {
