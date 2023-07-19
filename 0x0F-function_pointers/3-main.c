41a00706bba9763e66371b9f98dbe7391e27b078
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2023-07-19 14:50:44 -0700

    I CAN CALCULATE I HAVE A SUPERPOWER

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+
+/**
+*main- calculator
+*@argc: ...
+*@argv: ...
+*
+*Return: ...
+*/
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*cal)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (b == 0 && (strcmp(argv[2], "%") == 0 || strcmp(argv[2], "/") == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	cal = get_op_func(argv[2]);
+	if (cal == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = cal(a, b);
+	printf("%d\n", result);
+	return (0);
+}
