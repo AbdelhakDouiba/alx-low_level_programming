3862954308731eec1782dcb37f5361c9a6ad296e
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2023-07-19 15:19:51 -0700

    I CAN CALCULATE >>> I HAVE A SUPERPOWER

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,16 +19,16 @@ int main(int argc, char *argv[])
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	if (b == 0 && (strcmp(argv[2], "%") == 0 || strcmp(argv[2], "/") == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	cal = get_op_func(argv[2]);
 	if (!cal)
 	{
 		printf("Error\n");
 		exit(99);
+	}
+	if (b == 0 && (strcmp(argv[2], "%") == 0 || strcmp(argv[2], "/") == 0))
+	{
+		printf("Error\n");
+		exit(100);
 	}
 	result = cal(a, b);
 	printf("%d\n", result);
