60f617ad2f556e3518a2b80c968d58d895f44ecf
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2023-07-20 08:38:35 -0700

    To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,86 @@
+#include "variadic_functions.h"
+
+/**
+*print_all - prints anything.
+*@format: function parameter
+*
+*/
+
+void print_all(const char * const format, ...)
+{
+	int count = 0, i = 0, flag;
+	char *ptr;
+	va_list var;
+
+	count = get_count(format);
+	va_start(var, format);
+	while (format[i] != '\0')
+	{
+		flag = 1;
+		switch (format[i])
+		{
+			case 'i':
+				printf("%d", va_arg(var, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(var, double));
+				break;
+			case 'c':
+				printf("%c", va_arg(var, int));
+				break;
+			case 's':
+				ptr = va_arg(var, char *);
+				if (ptr == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", ptr);
+				break;
+			default:
+				flag = 0;
+				break;
+		}
+			print_separator(&count, flag);
+		i++;
+	}
+	printf("\n");
+	va_end(var);
+}
+
+/**
+*get_count - return the number of arguments
+*@form:function parameter
+*
+*Return: the number of argument
+*/
+int get_count(const char * const form)
+{
+	int i = 0, count = 0;
+
+	while (form[i] != '\0')
+	{
+		if (form[i] == 'i' || form[i] == 'f' || form[i] == 's' ||
+			form[i] == 'c')
+		{
+			count++;
+		}
+		i++;
+	}
+	return (count);
+}
+
+/**
+*print_separator - print the separator ", "
+*@a: function parameter
+*@b: function parameter
+*
+*/
+void print_separator(int *a, int b)
+{
+	if ((*a - 1) != 0 && b == 1)
+	{
+		printf(", ");
+		*a = *a - 1;
+	}
+}
