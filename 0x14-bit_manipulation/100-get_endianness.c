9093b76ca667e2b42d708ff9a8cfddfaa15d7e71
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2023-10-26 11:34:05 -0700

    ENDIANN ?

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*get_endianness - checks the endianness.
+*
+*/
+int get_endianness(void)
+{
+	unsigned int number = 1;
+	unsigned int *ptr = (unsigned char *)&num;
+
+	if(*ptr == 1)
+	{
+		return(1);
+	}
+	else
+	{
+		return (0);
+	}
+}
