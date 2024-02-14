712168c0039a0c44b8f10270e546d5aa9ae4615a
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-02-14 06:49:27 -0800

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	for (length = 0; text_content[length] != '\0'; length++)
 	{}
-	fd_write = write(fd_open, text_content, length - 1);
+	fd_write = write(fd_open, text_content, length);
 	if (fd_write == -1)
 		return (-1);
 	fd_close = close(fd_open);
-	if (fd_write == -1)
+	if (fd_close == -1)
 		return (-1);
 	return (1);
 }
