ef39d54fc3ad7f6115451d0888aeecdd96b83cea
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-02-14 06:51:47 -0800

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,8 +13,10 @@ int create_file(const char *filename, char *text_content)
 	int fd_open, fd_close, length;
 	ssize_t fd_write;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	fd_open = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd_open == -1)
 		return (-1);
