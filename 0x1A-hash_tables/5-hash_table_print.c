baae523cf3896c094e2f4ac274d81d98d44897db
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-28 09:22:08 -0700

    >>> print(ht)

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,6 +9,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *tmp;
 	unsigned long int i;
+	int flag = 0;
 
 	if (ht != NULL)
 	{
@@ -16,12 +17,15 @@ void hash_table_print(const hash_table_t *ht)
 		for (i = 0; i < ht->size; i++)
 		{
 			tmp = ht->array[i];
+			if (flag == 1 && tmp != NULL)
+				printf(", ");
 			while (tmp != NULL)
 			{
 				printf("'%s': '%s'", tmp->key, tmp->value);
 				if (tmp->next != NULL)
 					printf(", ");
 				tmp = tmp->next;
+				flag = 1;
 			}
 		}
 		printf("}\n");
